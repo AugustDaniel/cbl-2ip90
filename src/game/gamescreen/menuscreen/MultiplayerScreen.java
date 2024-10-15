2947951408e4750bf0026aeed092db0070d294fd
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2024-10-15 16:56:45 +0200

    added more server boilerplate

--- src/game/gamescreen/menuscreen/MultiplayerScreen.java
+++ src/game/gamescreen/menuscreen/MultiplayerScreen.java
@@ -1,14 +1,52 @@
 package game.gamescreen.menuscreen;
 
 import game.Game;
-import game.gamescreen.GameScreen;
+import game.GameState;
 
-import java.awt.*;
+import javax.swing.*;
 
 public class MultiplayerScreen extends MenuScreen {
 
+    private DefaultListModel<String> listModel;
+
     public MultiplayerScreen(Game game) {
         super(game);
+        listModel = new DefaultListModel<>();
+    }
+
+    @Override
+    protected void init() {
+        JButton hostButton = new JButton("Host new game");
+        JButton joinButton = new JButton("Join");
+        JButton refreshButton = new JButton("Refresh");
+        JButton backButton = new JButton("Back");
+        JList<String> lobbyList = new JList<>(listModel);
+
+        hostButton.addActionListener(e -> {
+
+        });
+
+        joinButton.addActionListener(e -> {
+
+        });
+
+        refreshButton.addActionListener(e -> {
+            refreshLobbies();
+        });
+
+        backButton.addActionListener(e -> {
+            game.setState(GameState.MODE_SELECTION);
+        });
+
+        add(hostButton);
+        add(joinButton);
+        add(refreshButton);
+        add(backButton);
+        add(lobbyList);
+    }
+
+    private void refreshLobbies() {
+        //TODO
     }
 
     @Override
--- src/server/Connection.java
+++ src/server/Connection.java
@@ -1,17 +1,67 @@
 package server;
 
+import game.ClientState;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public class Connection implements Runnable{
+public class Connection implements Runnable {
 
     private Socket socket;
+    private ObjectOutputStream output;
+    private ObjectInputStream input;
 
-    public Connection(Socket socket) {
+    public Connection(Socket socket) throws IOException {
         this.socket = socket;
+        this.output = new ObjectOutputStream(socket.getOutputStream());
+        this.input = new ObjectInputStream(socket.getInputStream());
+    }
+
+    public void disconnect() {
+        try {
+            if (socket != null) socket.close();
+            if (input != null) input.close();
+            if (output != null) output.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeObject(Object o) throws IOException {
+        output.writeObject(o);
+        output.flush();
+        output.reset();
+    }
+
+    private void writeBoolean(boolean bool) throws IOException {
+        output.writeBoolean(bool);
+        output.flush();
+        output.reset();
     }
 
     @Override
     public void run() {
+        while (true) {
+            try {
+                ClientState state = (ClientState) input.readObject();
+
+                switch (state) {
+                    case HOST -> hostLobby();
+                    case JOIN -> joinLobby();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void joinLobby() {
+
+    }
+
+    private void hostLobby() {
 
     }
 }
