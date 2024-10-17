024b3271a5d53e35b22c26d629a47ce8f3677576
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2024-10-17 12:40:08 +0200

    added bfs

--- src/game/pathfinding/Pathfinding.java
+++ src/game/pathfinding/Pathfinding.java
@@ -1,8 +1,34 @@
 package game.pathfinding;
 
 import java.awt.geom.Point2D;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
 
 public class Pathfinding {
+
+    public static Vertex startPoint;
+    public static Vertex goal;
+    public static Map<Vertex, Vertex> path;
+
+    public static void bfs() {
+        Queue<Vertex> frontier = new ArrayDeque<>();
+        frontier.offer(startPoint);
+        Map<Vertex, Vertex> cameFrom = new HashMap<>();
+        cameFrom.put(startPoint, null);
+
+        while (!frontier.isEmpty()) {
+            Vertex current = frontier.poll();
+
+            for (Vertex next : current.getNeighbours()) {
+                if (!cameFrom.containsKey(next)) {
+                    frontier.offer(next);
+                    cameFrom.put(next, current);
+                }
+            }
+        }
+    }
 
     public static Point2D getEndPoint() {
         return new Point2D.Double(200, 100);
--- src/game/pathfinding/Vertex.java
+++ src/game/pathfinding/Vertex.java
@@ -1,4 +1,22 @@
 package game.pathfinding;
 
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Vertex {
+
+    private List<Vertex> neighbours;
+
+    public Vertex(Point2D position) {
+        this.neighbours = new ArrayList<>();
+    }
+
+    public void addNeighbour(Vertex vertex) {
+        neighbours.add(vertex);
+    }
+
+    public List<Vertex> getNeighbours() {
+        return neighbours;
+    }
 }
